e7bd3d31ba95bc0d2e3814d0645d117074022967
foreverich <1365274496@qq.com>
2016-02-20 18:15:25 +0800

    mkiso.sh

--- centos_iso/mkiso.sh
+++ centos_iso/mkiso.sh
@@ -6,13 +6,13 @@ else
 	version=$1
 fi
 
-rm -f /tmp/CentOS-5.6-HaoDou-$version.iso
+rm -f /tmp/CentOS-6.7-HaoDou-$version.iso
 
 cd /los
 declare -x discinfo=`head -1 .discinfo`
 createrepo -u "media://$discinfo" -g repodata/comps.xml .
 
-mkisofs -R -J -T -r -l -d -allow-multidot -allow-leading-dots -no-bak -o /tmp/CentOS-5.6-HaoDou-$version.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table .
+mkisofs -R -J -T -r -l -d -allow-multidot -allow-leading-dots -no-bak -o /tmp/CentOS-6.7-HaoDou-$version.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table .
 
-/usr/lib/anaconda-runtime/implantisomd5 /tmp/CentOS-5.6-HaoDou-$version.iso
+/usr/lib/anaconda-runtime/implantisomd5 /tmp/CentOS-6.7-HaoDou-$version.iso
 
